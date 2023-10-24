05574406f04ba3ef6439ef64f96b3159ef4459a0
pratikrawat <pratikrawat27@gmail.com>
2023-10-24 17:11:20 +0530

    new

---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,7 @@
+function Chai() {
+    return(
+        <h2>Pratik chai is ready</h2>
+    )
+}
+
+export default Chai;
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return (
+        <h2>Pratik you chai is ready</h2>
+    )
+}
+export default Chai
